41f5a0bd76975c68886408f3b63ffd87ee627be6
baaamk <brandon55321@gmail.com>
2025-11-16 01:23:40 +0900

    docs: class 정리

---
+++ 2_class_and_oop/1_class_keyword.js
@@ -0,0 +1,28 @@
+/**
+ * Class Keyword
+ */
+
+class IdolModel{
+    name;
+    year;
+
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+}
+
+//constructor - 생성자
+
+const yujin = new IdolModel();
+console.log(yujin);
+
+class IdolModel{
+    name;
+    year;
+
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+}
